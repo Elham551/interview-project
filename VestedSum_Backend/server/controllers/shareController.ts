8baf22d9099a133ec5a192965da70f9850cb50d3
Elham Souri <ElhamSouri@outlook.com>
2025-02-10 17:36:31 +0100

    add Post Employee

--- VestedSum_Backend/server/controllers/shareController.ts
+++ VestedSum_Backend/server/controllers/shareController.ts
@@ -33,3 +33,32 @@ const shareGrants = [
 export const getShares = (c: Context) => {
   return c.json(shareProcessor(shareGrants));
 };
+
+// add shares
+export const processShares = async (ctx: Context) => {
+  const body = await ctx.req.json();
+
+  // Validate the request body
+  if (
+    !body.employeeId || !body.employeeName || !body.grantedShares ||
+    !body.vestedShares
+  ) {
+    ctx.status(500);
+    ctx.json({ error: "all fields are required" });
+    return;
+  }
+
+  const newEmployee = {
+    employeeId: body.employeeId,
+    employeeName: body.employeeName,
+    grantedShares: body.grantedShares,
+    vestedShares: body.vestedShares,
+  };
+  shareGrants.push(newEmployee);
+  console.log(shareGrants);
+
+  return ctx.json({
+    message: "Employee added successfully",
+    share: shareGrants,
+  });
+};
--- VestedSum_Backend/server/routes/routes.ts
+++ VestedSum_Backend/server/routes/routes.ts
@@ -1,8 +1,9 @@
 import { Hono } from "https://deno.land/x/hono@v4.3.11/mod.ts";
-import { getShares } from "../controllers/shareController.ts";
+import { getShares, processShares } from "../controllers/shareController.ts";
 
 const route = new Hono();
 
 route.get("/api/shares", getShares);
+route.post("/api/shares", processShares);
 
 export default route;
